2d38ff010837109deef489207ae62959f84d3f07
Sambath N <sambathsain1@gmail.com>
2025-11-14 14:13:57 +0530

    Eric project testcase

---
+++ src/main/java/Proj/ericsson/App.java
@@ -0,0 +1,13 @@
+package Proj.ericsson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
